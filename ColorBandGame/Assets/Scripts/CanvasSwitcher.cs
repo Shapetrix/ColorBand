da3a0e8e201cc4add81eec62e6978d455c88d17e
MRBATSHAPETRIX\Brandon Humfleet <shapetrix@gmail.com>
2019-11-14 20:17:05 -0600

    adding Menu Control
    adding in ruff menus

---
+++ ColorBandGame/Assets/Scripts/CanvasSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class CanvasSwitcher : MonoBehaviour
+{
+    public CanvasType desiredCavasType;
+
+    CanvasManager canvasManager;
+    Button menuButton;
+
+    private void Start()
+    {
+        menuButton = GetComponent<Button>();
+        menuButton.onClick.AddListener(OnButtonClicked);
+        canvasManager = CanvasManager.GetInstance();
+    }
+
+    void OnButtonClicked()
+    {
+        canvasManager.SwitchCanvas(desiredCavasType);
+    }
+}
